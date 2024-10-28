c09e837e0f3d1f1a8c10d032797ba3005f3bf401
Nguyen Phuong Hoang <hoang093898xxx@gmail.com>
2024-10-28 15:37:45 +0700

    hay tha cho toi

--- src/app.ts
+++ src/app.ts
@@ -19,9 +19,9 @@ import orderRoutes from './routes/orderRoutes';
 import connectDB from './config/database';
 import dotenv from 'dotenv';
 dotenv.config();
-console.log("PAYOS_CLIENT_ID:", process.env.PAYOS_CLIENT_ID);
-console.log("PAYOS_API_KEY:", process.env.PAYOS_API_KEY);
-console.log("PAYOS_CHECKSUM_KEY:", process.env.PAYOS_CHECKSUM_KEY);
+//console.log("PAYOS_CLIENT_ID:", process.env.PAYOS_CLIENT_ID);
+//console.log("PAYOS_API_KEY:", process.env.PAYOS_API_KEY);
+//console.log("PAYOS_CHECKSUM_KEY:", process.env.PAYOS_CHECKSUM_KEY);
 
 const cors = require('cors');
 
