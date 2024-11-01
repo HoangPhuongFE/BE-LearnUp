57cd9bb2dbd40ef1113ecd9c0ee673e7feb693eb
Nguyen Phuong Hoang <hoang093898xxx@gmail.com>
2024-11-01 14:39:05 +0700

    HTTPS BE

--- dist/server.js
+++ dist/server.js
@@ -3,27 +3,35 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const https_1 = __importDefault(require("https"));
+const fs_1 = __importDefault(require("fs"));
 const app_1 = __importDefault(require("./app"));
 const socket_io_1 = require("socket.io");
-const socket_1 = require("./socket/socket");
-// Tạo HTTP server
 const PORT = process.env.PORT || 8080;
-const httpServer = require('http').createServer(app_1.default);
-// Tạo server Socket.IO
-const io = new socket_io_1.Server(httpServer, {
+let server;
+if (process.env.NODE_ENV === 'production') {
+    // Sử dụng HTTPS trên server sản xuất (VPS)
+    const sslOptions = {
+        key: fs_1.default.readFileSync('/etc/letsencrypt/live/learnup.work/privkey.pem'),
+        cert: fs_1.default.readFileSync('/etc/letsencrypt/live/learnup.work/fullchain.pem')
+    };
+    server = https_1.default.createServer(sslOptions, app_1.default);
+    console.log('Running with HTTPS');
+}
+else {
+    // Sử dụng HTTP cho phát triển cục bộ
+    server = http_1.default.createServer(app_1.default);
+    console.log('Running with HTTP');
+}
+// Thiết lập Socket.IO với server
+const io = new socket_io_1.Server(server, {
     cors: {
-        origin: '*',
+        origin: process.env.NODE_ENV === 'production' ? 'https://learnup.work' : 'http://localhost:3000',
         methods: ['GET', 'POST']
     }
 });
-// Thiết lập socket
-(0, socket_1.setupSocket)(io);
 // Lắng nghe cổng
-/*
-httpServer.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-*/
-httpServer.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
